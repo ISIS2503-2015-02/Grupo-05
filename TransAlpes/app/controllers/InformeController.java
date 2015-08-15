1dd9763bf8c6127c0213f6894e8d12206862191a
aiMojica10 <ai.mojica10@uniandes.edu.co>
2015-08-15 12:45:27 -0500

    get/general de informes

--- TransAlpes/app/controllers/InformeController.java
+++ TransAlpes/app/controllers/InformeController.java
@@ -1,10 +1,15 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import models.Estacion;
+import models.Informe;
 import play.db.ebean.Transactional;
+import play.libs.Json;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.util.List;
 
 /**
  * Created by ai.mojica10 on 13/08/2015.
@@ -28,4 +33,12 @@ public Result darReporte()
  return ok("usted ha solicitado los infomes");
 }
 
+@Transactional
+@BodyParser.Of(BodyParser.Json.class)
+public Result darInformes()
+{
+	JsonNode json = request().body().asJson();
+	List<Informe> list = Informe.find.all();
+	return ok(Json.toJson(list));
+}
 }
