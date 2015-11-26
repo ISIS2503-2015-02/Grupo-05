7540feff39421cab2b4f276b346c9aec702b4f5f
SamuelSSalazar <samuel.s.salazar@gmail.com>
2015-11-25 21:59:34 -0500

    .

--- StandAlones/src/logica/TBC.java
+++ StandAlones/src/logica/TBC.java
@@ -3,13 +3,15 @@ package logica;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
 
 import com.google.code.geocoder.model.LatLng;
 import com.google.gson.JsonObject;
+
+import sun.net.www.protocol.http.HttpURLConnection;
+import org.apache.commons.codec.binary.Base64;
+
 
 
 
@@ -31,37 +33,18 @@ public class TBC
 
 	public boolean login(String id) throws Exception
 	{
-		String url = "https://arqui201326232.herokuapp.com/vehiculos/"+id;
+		String url = "http://arquisoft20152.herokuapp.com/webresources/users/login";
 
-		URL obj = new URL(url);
-		sun.net.www.protocol.https.HttpsURLConnectionImpl con = (sun.net.www.protocol.https.HttpsURLConnectionImpl) obj.openConnection();
 
-		BufferedReader in = new BufferedReader( new InputStreamReader(con.getInputStream()));
-		String linea;
-		StringBuilder response = new StringBuilder();
-
-		while ((linea = in.readLine()) != null) {
-			response.append(linea);
-		}
-		in.close();
-		this.id = id;
-		return con.getResponseCode()== HttpsURLConnection.HTTP_OK;
-	}
-	
-	public String reportarUbicacion(LatLng pos, String estado, int km) throws Exception
-	{
 		JsonObject json = new JsonObject();
-		json.addProperty("latitud", pos.getLat().doubleValue());
-		json.addProperty("longitud", pos.getLat().doubleValue());
-		json.addProperty("hora", System.currentTimeMillis());
-		json.addProperty("estado", estado);
-		json.addProperty("kilometraje", km);
+		json.addProperty("userName", "vehiculoUser");
+		json.addProperty("password", "Vehiculo123");
 
 
-		String url = "https://arqui201326232.herokuapp.com/vehiculos/"+id+"/posiciones/agregar";
+
 		System.out.println(url);
 		URL obj = new URL(url);
-		sun.net.www.protocol.https.HttpsURLConnectionImpl con = (sun.net.www.protocol.https.HttpsURLConnectionImpl) obj.openConnection();
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		con.setRequestMethod("POST");
 		con.setRequestProperty("Content-Type", "application/json");
 		
@@ -81,7 +64,52 @@ public class TBC
 			response.append(linea);
 		}
 		in.close();
+		
+		
+		this.id = id;
+		System.out.println(response);
+		return con.getResponseCode()== HttpURLConnection.HTTP_OK;
+	}
+	
+	public String reportarUbicacion(LatLng pos, String estado, int km) throws Exception
+	{
+		String url = "http://arquisoft20152.herokuapp.com/webresources/vehiculos/"+id+"/posiciones";
+		
+		JsonObject json = new JsonObject();
+		json.addProperty("latitud", pos.getLat().doubleValue());
+		json.addProperty("longitud", pos.getLat().doubleValue());
+		json.addProperty("hora", System.currentTimeMillis());
+		json.addProperty("estado", estado);
+		json.addProperty("kilometraje", km);
 
+
+		System.out.println(url);
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", "application/json");
+		
+		String encoding = Base64.encodeBase64String ("admin:Admin123".getBytes());
+		con.setRequestProperty("Authorization", "Basic " + encoding);
+		
+		String params = json.toString();
+
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(params);
+		wr.flush();
+		wr.close();
+
+		BufferedReader in = new BufferedReader( new InputStreamReader(con.getInputStream()));
+		String linea;
+		StringBuilder response = new StringBuilder();
+
+		while ((linea = in.readLine()) != null) {
+			response.append(linea);
+		}
+		in.close();
+		
+		System.out.println(response);
 		return response.toString();
 	}
 }
